11db460e9231c99fa27905cb3eeac3dd303fc05b
Felipe Lima Silva <felipe.lima.silva1993@gmail.com>
2024-02-22 23:33:15 -0300

    add block define acl to block ou allow bucket to public

--- s3.tf
+++ s3.tf
@@ -3,3 +3,13 @@ resource "aws_s3_bucket" "this" {
   bucket = var.bucket_env_name
   tags   = var.tags
 }
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  count  = var.enable_create_s3_bucket_log == true ? 1 : 0
+  bucket = aws_s3_bucket.this[0].id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
