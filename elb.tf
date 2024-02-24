d9cf3fc0a06fb0f769b6d0f08f084ff0dc851908
Felipe Lima Silva <felipe.lima.silva1993@gmail.com>
2024-02-24 20:35:18 -0300

    create listener http and create rule default to redirect to https

--- elb.tf
+++ elb.tf
@@ -16,3 +16,23 @@ resource "aws_lb" "this" {
 
   tags = merge(var.tags, var.aditional_tags)
 }
+
+
+resource "aws_lb_listener" "redirect" {
+  count = var.create_rule_redirect_https == true ? 1 : 0
+
+  load_balancer_arn = aws_lb.this.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+  tags = merge(var.tags, var.aditional_tags)
+}
