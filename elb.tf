6eb9899de123fa5af06856cf662624cd618cccd3
Felipe Lima Silva <felipe.lima.silva1993@gmail.com>
2024-02-24 20:30:38 -0300

    add block to specify access log, add line to specify tags merging to aditional tags and add protection to delete

--- elb.tf
+++ elb.tf
@@ -5,4 +5,14 @@ resource "aws_lb" "this" {
   security_groups    = [aws_security_group.this.id]
   subnets            = var.subnets_ids
 
+  enable_deletion_protection = var.enable_deletion_protection
+
+  access_logs {
+    bucket  = var.enable_create_s3_bucket_log == true ? aws_s3_bucket.this[0].id : ""
+    prefix  = var.access_logs_prefix
+    enabled = var.enable_create_s3_bucket_log
+  }
+  depends_on = [aws_s3_bucket.this]
+
+  tags = merge(var.tags, var.aditional_tags)
 }
